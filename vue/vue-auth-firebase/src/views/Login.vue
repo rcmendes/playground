ce549f2335757fbce190445ce4fe5ec9b7e4955a
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2019-11-05 07:17:37 -0300

    init commit.

---
+++ vue/vue-auth-firebase/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1>Login</h1>
+    <input type="text" name="email" placeholder="E-mail" />
+    <br />
+    <input type="password" name="password" placeholder="Password" />
+    <br />
+    <div class="btn-login">
+      <button>Sign in</button>
+    </div>
+    <p>
+      Don't have an account yet?
+      <a href="#">Create an account</a>
+    </p>
+  </main>
+</template>
+<script>
+export default {
+  data: () => ({})
+};
+</script>
