fd081e8c7c7081e29c97d0cfde60e03b88743017
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2020-02-23 21:18:33 -0300

    check credentials from user table on database

--- python/mytodo/resources/login.py
+++ python/mytodo/resources/login.py
@@ -1,24 +1,31 @@
+import bcrypt
 from flask import request, jsonify
 from flask_restful import Resource, request
 from flask_jwt_extended import create_access_token
+
+from models.user import UserModel
 
 
 class Login(Resource):
     @classmethod
     def post(cls):
         if not request.is_json:
-            return jsonify({"msg": "Missing JSON in request"}), 400
+            return jsonify({"message": "Missing JSON in request"}), 400
 
-        username = request.json.get('username', None)
+        email = request.json.get('email', None)
         password = request.json.get('password', None)
-        if not username:
-            return jsonify({"msg": "Missing username parameter"}), 400
+        if not email:
+            return jsonify({"message": "Missing e-mail parameter"}), 400
         if not password:
-            return jsonify({"msg": "Missing password parameter"}), 400
+            return jsonify({"message": "Missing password parameter"}), 400
 
-        if username != 'test' or password != 'test':
-            return jsonify({"msg": "Bad username or password"}), 401
+        user = UserModel.find_by_email(email)
+        if not user:
+            return {"message": "User credentials (email/password) are incorrect."}, 401
+
+        if not bcrypt.checkpw(password.encode('utf8'), user.password):
+            return {"message": "User credentials (email/password) are incorrect."}, 401
 
         # Identity can be any data that is json serializable
-        access_token = create_access_token(identity=username)
+        access_token = create_access_token(identity=user.id)
         return {"access_token": access_token}, 200
