e7da1222517e6cbd1698373a3d63f54b06b4b1a6
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2020-02-26 16:14:39 -0300

    added default value for JWT_SECRET_KEY.

--- python/mytodo/backend/config.py
+++ python/mytodo/backend/config.py
@@ -4,4 +4,4 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///data.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-me!")
