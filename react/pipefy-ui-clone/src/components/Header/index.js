0233a06d4ebfa792c83703721a0332b4fab95dc3
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2020-04-05 07:12:42 -0300

    init commit.

---
+++ react/pipefy-ui-clone/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default () => {
+	return (
+		<Container>
+			<h1>Flow</h1>
+		</Container>
+	);
+};
---
+++ react/pipefy-ui-clone/src/components/List/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Container } from "./styles";
+import Card from "../Card";
+import { MdAdd } from "react-icons/md";
+
+export default ({ data, index: listIndex }) => {
+	return (
+		<Container done={data.done}>
+			<header>
+				<h2>{data.title}</h2>
+				{data.creatable && (
+					<button>
+						<MdAdd size={24} color="#fff" />
+					</button>
+				)}
+			</header>
+			<ul>
+				{data.cards.map((card, index) => (
+					<Card
+						key={card.id}
+						listIndex={listIndex}
+						index={index}
+						data={card}
+					/>
+				))}
+			</ul>
+		</Container>
+	);
+};
