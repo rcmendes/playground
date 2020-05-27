bf008932c3191a742da7bf105567df487969015f
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2020-05-27 06:39:15 -0300

    init commit

--- rust/traits/src/main.rs
+++ rust/traits/src/main.rs
@@ -18,3 +18,8 @@ fn main() {
 
     service.repo.print();
 }
+
+//References
+// - https://stackoverflow.com/questions/40064700/vector-of-generic-structs-in-rust
+// - https://tratt.net/laurie/blog/entries/a_quick_look_at_trait_objects_in_rust.html
+// - https://brson.github.io/rust-anthology/1/all-about-trait-objects.html
